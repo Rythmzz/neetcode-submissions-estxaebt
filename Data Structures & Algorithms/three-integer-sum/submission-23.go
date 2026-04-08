0de8d9d8bce491822a6b0a20eeb2b831ecb02515
Rythmzz <101648696+Rythmzz@users.noreply.github.com>
2026-04-08 08:47:46 +0700

    Add: three-integer-sum - submission-23

---
+++ Data Structures & Algorithms/three-integer-sum/submission-23.go
@@ -0,0 +1,38 @@
+func threeSum(nums []int) [][]int {
+	sort.Ints(nums)
+	result := [][]int{}
+	for i:= 0 ;i< len(nums) - 2;i++ {
+		if i > 0 && nums[i] == nums[i-1] {
+			continue
+		}
+
+		j,k := i+1, len(nums)-1
+
+		for j < k {
+			sum := nums[i] + nums[j] + nums[k]
+
+			if sum > 0 {
+				k--
+			} else if sum < 0 {
+				j++
+			} else {
+
+				for j < k && nums[j] == nums[j+1] {
+					j++
+				}
+
+				for j < k && nums[k] == nums[k-1] {
+					k--
+				}
+
+				arr := []int{nums[i],nums[j],nums[k]}
+				result = append(result,arr)
+
+				j++
+				k--
+			}
+		}
+	}
+
+	return result
+}
