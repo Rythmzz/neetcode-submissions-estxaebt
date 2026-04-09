50f0791bb05aaea3f2d7197e96151f9e805c6879
Rythmzz <101648696+Rythmzz@users.noreply.github.com>
2026-04-09 08:53:54 +0700

    Add: subtree-of-a-binary-tree - submission-6

---
+++ Data Structures & Algorithms/subtree-of-a-binary-tree/submission-6.go
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+func isSubtree(root *TreeNode, subRoot *TreeNode) bool {
+    if root == nil {
+		return false
+	} 
+
+	if same(root,subRoot) {
+		return true
+	}
+
+	return isSubtree(root.Left,subRoot) || isSubtree(root.Right,subRoot)
+}
+
+func same(p,q *TreeNode) bool {
+	if p == nil && q == nil {
+		return true
+	}
+
+	if p != nil && q != nil {
+		if p.Val != q.Val {
+			return false
+		}
+	} else {
+		return false
+	}
+
+	return same(p.Left,q.Left) && same(p.Right,q.Right)
+}
