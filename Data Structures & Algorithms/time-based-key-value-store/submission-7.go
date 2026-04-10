ede6bf864d9bd818916c67d80cef5b903b6850cc
Rythmzz <101648696+Rythmzz@users.noreply.github.com>
2026-04-10 09:10:02 +0700

    Add: time-based-key-value-store - submission-7

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-7.go
@@ -0,0 +1,36 @@
+type TimeMap struct {
+	store map[string][]Pair
+}
+
+type Pair struct {
+	val string
+	timestamp int
+}
+
+func Constructor() TimeMap {
+	return TimeMap{
+		store: map[string][]Pair{},
+		}
+}
+
+func (this *TimeMap) Set(key string, value string, timestamp int) {
+	this.store[key] = append(this.store[key],Pair{val: value,timestamp: timestamp})
+}
+
+func (this *TimeMap) Get(key string, timestamp int) string {
+	arr := this.store[key]
+	value := ""
+	l,r := 0, len(arr)-1
+	for l <= r {
+		mid := l + (r-l)/2
+
+		if arr[mid].timestamp > timestamp {
+			r = mid - 1
+		} else {
+			value = arr[mid].val
+			l = mid + 1
+		}
+	}
+
+	return value
+}
