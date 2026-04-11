e655c02053b34875b5c42776d7668bfcf7734055
Rythmzz <101648696+Rythmzz@users.noreply.github.com>
2026-04-11 08:46:26 +0700

    Add: valid-binary-search-tree - submission-10

---
+++ Data Structures & Algorithms/valid-binary-search-tree/submission-10.go
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+func isValidBST(root *TreeNode) bool {
+    return dfs(root,math.MinInt64,math.MaxInt64)
+}
+
+func dfs(root *TreeNode, minVal,maxVal int) bool {
+	if root == nil {
+		return true
+	}
+
+	if root.Val <= minVal || root.Val >= maxVal {
+		return false
+	}
+
+	return dfs(root.Left,minVal,root.Val) && dfs(root.Right,root.Val,maxVal)
+}
