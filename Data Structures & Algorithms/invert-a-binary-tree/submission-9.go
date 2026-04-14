7ddfe4f94883ce0b185312b5d44da75a8ccef18f
Rythmzz <101648696+Rythmzz@users.noreply.github.com>
2026-04-14 09:47:59 +0700

    Add: invert-a-binary-tree - submission-9

---
+++ Data Structures & Algorithms/invert-a-binary-tree/submission-9.go
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+func invertTree(root *TreeNode) *TreeNode {
+
+	curr := root
+	invert(curr)
+
+	return root
+}
+
+func invert(curr *TreeNode) {
+	if curr == nil {
+		return
+	}
+
+	if curr.Left != nil || curr.Right != nil {
+		curr.Left, curr.Right = curr.Right,curr.Left
+	}
+
+	invert(curr.Left)
+	invert(curr.Right)
+}
