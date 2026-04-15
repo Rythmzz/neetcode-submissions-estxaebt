263a3894e118a19c3fbe32e88b98a23b0d0910a8
Rythmzz <101648696+Rythmzz@users.noreply.github.com>
2026-04-15 08:56:34 +0700

    Add: string-encode-and-decode - submission-18

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-18.go
@@ -0,0 +1,30 @@
+type Solution struct{}
+
+func (s *Solution) Encode(strs []string) string {
+	var en strings.Builder
+
+	for _, str := range strs {
+		length := fmt.Sprintf("%04d",len(str))
+		en.WriteString(length)
+		en.WriteString(str)
+	}
+
+	return en.String()
+}
+
+func (s *Solution) Decode(encoded string) []string {
+	i:=0
+	result := []string{}
+	for i < len(encoded) {
+		lengthStr := encoded[i:i+4]
+		length,_ := strconv.Atoi(lengthStr)
+
+		i += 4
+
+		result = append(result,encoded[i:i+length])
+
+		i+= length
+	}
+
+	return result
+}
