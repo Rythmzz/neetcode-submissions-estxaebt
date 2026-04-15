9d25ec4b21180bb312f97f168cbc69a5234f4112
Rythmzz <101648696+Rythmzz@users.noreply.github.com>
2026-04-15 08:58:20 +0700

    Add: products-of-array-discluding-self - submission-12

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-12.go
@@ -0,0 +1,17 @@
+func productExceptSelf(nums []int) []int {
+	mul := 1
+	result := make([]int,len(nums))
+
+	for i:= 0;i< len(result);i++ {
+		result[i] = mul
+		mul *= nums[i]
+	}
+
+	mul = 1
+	for i:= len(nums)-1;i >= 0;i-- {
+		result[i] *= mul
+		mul *= nums[i]
+	}
+
+	return result
+}
