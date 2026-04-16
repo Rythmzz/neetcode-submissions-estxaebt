2cab9afbfc6962d01a69640f8b1401005c978ddf
Rythmzz <101648696+Rythmzz@users.noreply.github.com>
2026-04-16 09:24:49 +0700

    Add: max-water-container - submission-15

---
+++ Data Structures & Algorithms/max-water-container/submission-15.go
@@ -0,0 +1,21 @@
+func maxArea(heights []int) int {
+	l,r := 0, len(heights)-1
+	maxArea := 0
+
+	for l < r {
+		area := (r-l)*min(heights[l],heights[r])
+		maxArea = max(maxArea,area)
+
+		if heights[l] > heights[r] {
+			r--
+		} else if heights[l] < heights[r] {
+			l++
+		} else {
+			r--
+			l++
+		}
+	
+	}
+
+	return maxArea
+}
