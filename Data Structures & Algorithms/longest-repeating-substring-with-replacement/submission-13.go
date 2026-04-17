d862baaf03fd4c44a707ad9666747ac5d320eb80
Rythmzz <101648696+Rythmzz@users.noreply.github.com>
2026-04-17 10:11:51 +0700

    Add: longest-repeating-substring-with-replacement - submission-13

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-13.go
@@ -0,0 +1,23 @@
+func characterReplacement(s string, k int) int {
+	freq := map[byte]int{}
+	maxFreq := 0
+	maxLength := 0
+
+	l := 0
+	for r:=0;r<len(s);r++ {
+		ch := s[r]
+
+		freq[ch]++
+		maxFreq = max(maxFreq,freq[ch])
+		windowLen := r-l+1
+
+		if windowLen - maxFreq > k {
+			freq[s[l]]--
+			l++
+		}
+
+		maxLength = max(maxLength,r-l+1)
+	}
+
+	return maxLength
+}
