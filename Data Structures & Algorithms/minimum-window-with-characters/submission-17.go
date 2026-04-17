ba7eaf3fd89c62be645a640e3f7c3025f991a083
Rythmzz <101648696+Rythmzz@users.noreply.github.com>
2026-04-17 10:23:57 +0700

    Add: minimum-window-with-characters - submission-17

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-17.go
@@ -0,0 +1,48 @@
+func minWindow(s string, t string) string {
+	if len(t) > len(s) {
+		return ""
+	}
+
+    need := map[byte]int{}
+	for i:= 0; i< len(t);i++ {
+		need[t[i]]++
+	}
+
+	window := map[byte]int{}
+	l,r,start,valid,length := 0,0,0,0,len(s)+1
+
+	for r < len(s) {
+		ch := s[r]
+		r++
+
+		window[ch]++
+		if window[ch] == need[ch] {
+			valid++
+		}
+
+		for len(need) == valid {
+			if r - l < length {
+				start = l
+				length = r - l
+			}
+
+			dh := s[l]
+			l++
+
+			if need[dh] > 0 {
+				if window[dh] == need[dh] {
+					valid--
+				}
+				window[dh]--
+			}
+		}
+	}
+
+	if length == len(s)+1 {
+		return ""
+	}
+
+	return s[start:start+length]
+
+
+}
