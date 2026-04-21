aa385ff92e6a782b3084ed33360ae76e017c3117
Rythmzz <101648696+Rythmzz@users.noreply.github.com>
2026-04-21 08:43:14 +0700

    Add: reverse-a-linked-list - submission-8

---
+++ Data Structures & Algorithms/reverse-a-linked-list/submission-8.go
@@ -0,0 +1,20 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+
+func reverseList(head *ListNode) *ListNode {
+    var dummy *ListNode
+
+	for head != nil {
+		next := head.Next
+		head.Next = dummy
+		dummy = head
+		head = next
+	}
+
+	return dummy
+}
