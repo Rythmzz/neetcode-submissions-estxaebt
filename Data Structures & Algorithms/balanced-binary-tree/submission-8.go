ce7cab3874b1d18d6fd9383a76b478f7ae8282fe
Rythmzz <101648696+Rythmzz@users.noreply.github.com>
2026-04-24 09:33:28 +0700

    Add: balanced-binary-tree - submission-8

---
+++ Data Structures & Algorithms/balanced-binary-tree/submission-8.go
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+func isBalanced(root *TreeNode) bool {
+    _, h:= balance(root)
+	if h {
+		return true
+	}
+
+	return false
+}
+
+func balance(curr *TreeNode) (int,bool) {
+	if curr == nil {
+		return 0, true
+	}
+
+	leftDepth, left := balance(curr.Left)
+	if !left {
+		return 0, false
+	}
+
+	rightDepth, right := balance(curr.Right)
+	if !right {
+		return 0, false
+	}
+
+	if abs(rightDepth - leftDepth) > 1 {
+		return 0, false
+	}
+
+	return 1+max(rightDepth,leftDepth),true
+
+}
+
+func abs(n int) int {
+	if n < 0 {return -n}
+	return n
+}
