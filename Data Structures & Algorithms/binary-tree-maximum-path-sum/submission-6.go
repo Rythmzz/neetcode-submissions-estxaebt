2eaeed1c761478bacc6210cb7be58a0a6e95525c
Rythmzz <101648696+Rythmzz@users.noreply.github.com>
2026-05-05 15:35:04 +0700

    Add: binary-tree-maximum-path-sum - submission-6

---
+++ Data Structures & Algorithms/binary-tree-maximum-path-sum/submission-6.go
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+func maxPathSum(root *TreeNode) int {
+	maxCurrent := root.Val
+	dfs(root,&maxCurrent)
+	return maxCurrent
+}
+
+func dfs(curr *TreeNode, maxCurrent *int) int {
+	if curr == nil {
+		return 0
+	}
+
+	lD := max(0,dfs(curr.Left,maxCurrent))
+	rD := max(0,dfs(curr.Right,maxCurrent))
+
+	sum := curr.Val + lD + rD
+
+	if sum > *maxCurrent {
+		*maxCurrent = sum
+	}
+
+	return curr.Val + max(lD,rD)
+}
