27e5a5e15e09d8b4f54c1eec6478fe331e02d001
Rythmzz <101648696+Rythmzz@users.noreply.github.com>
2026-05-06 09:50:25 +0700

    Add: longest-consecutive-sequence - submission-14

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-14.go
@@ -0,0 +1,20 @@
+func longestConsecutive(nums []int) int {
+	start := map[int]bool{}
+	maxCount :=0
+
+	for _, n:= range nums {
+		start[n] = true
+	}
+
+	for _, n:= range nums {
+		if !start[n-1] {
+			count := 1
+			for start[n+count] {
+				count++
+			}
+			maxCount = max(maxCount,count)
+		}
+	}
+
+	return maxCount
+}
