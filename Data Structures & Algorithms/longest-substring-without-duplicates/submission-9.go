29919ab18dd48b20af4e81a2678ad2c47ab6e96e
Rythmzz <101648696+Rythmzz@users.noreply.github.com>
2026-05-08 08:40:33 +0700

    Add: longest-substring-without-duplicates - submission-9

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-9.go
@@ -0,0 +1,20 @@
+func lengthOfLongestSubstring(s string) int {
+	lastSeen := map[byte]int{}
+	maxLength := 0
+
+	l := 0
+	for r := 0;r < len(s);r++ {
+		ch := s[r]
+
+		if idx, exist := lastSeen[ch]; exist {
+			if idx >= l {
+				l = idx + 1
+			}
+		}
+
+		lastSeen[ch] = r
+		maxLength = max(maxLength,r-l+1)
+	}
+
+	return maxLength
+}
