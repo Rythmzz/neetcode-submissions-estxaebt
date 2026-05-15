4eb9dd4bd0d15f69f42f5e20552e8b81200188d9
Rythmzz <101648696+Rythmzz@users.noreply.github.com>
2026-05-15 09:44:34 +0700

    Add: minimum-stack - submission-14

---
+++ Data Structures & Algorithms/minimum-stack/submission-14.go
@@ -0,0 +1,36 @@
+type MinStack struct {
+	s []int
+	m []int
+}
+
+func Constructor() MinStack {
+	return MinStack{}
+}
+
+func (this *MinStack) Push(val int) {
+	this.s = append(this.s, val)
+
+	if len(this.m) > 0 { 
+		if this.m[len(this.m)-1] >= val {
+			this.m = append(this.m,val)
+		}
+	} else {
+		this.m = append(this.m,val)
+	}
+}
+
+func (this *MinStack) Pop() {
+	if this.s[len(this.s)-1] == this.m[len(this.m)-1] {
+		this.m = this.m[:len(this.m)-1]
+	}
+
+	this.s = this.s[:len(this.s)-1]
+}
+
+func (this *MinStack) Top() int {
+	return this.s[len(this.s)-1]
+}
+
+func (this *MinStack) GetMin() int {
+	return this.m[len(this.m)-1]
+}
