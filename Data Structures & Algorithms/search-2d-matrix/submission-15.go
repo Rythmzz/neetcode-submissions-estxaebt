8c3d8cb3a7b6b8476f6b580d0264658d34775435
Rythmzz <101648696+Rythmzz@users.noreply.github.com>
2026-05-20 13:37:31 +0700

    Add: search-2d-matrix - submission-15

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-15.go
@@ -0,0 +1,30 @@
+func searchMatrix(matrix [][]int, target int) bool {
+	
+	for i:= 0; i< len(matrix);i++ {
+		if binarySearch(matrix[i],target) {
+			return true
+		}
+	}
+
+	return false
+}
+
+func binarySearch(nums []int, target int) bool {
+	l, r := 0, len(nums)-1
+
+	for l <= r {
+		mid := l + (r-l)/2
+
+		if nums[mid] == target {
+			return true
+		}
+
+		if nums[mid] > target {
+			r = mid - 1
+		} else {
+			l = mid + 1
+		}
+	}
+
+	return false
+}
