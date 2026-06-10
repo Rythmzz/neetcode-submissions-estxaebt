32e81394ea7b09e3ddb3cc7ad22b9a687f66584e
Rythmzz <101648696+Rythmzz@users.noreply.github.com>
2026-06-10 14:55:08 +0700

    Add: two-integer-sum-ii - submission-27

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-27.go
@@ -0,0 +1,19 @@
+func twoSum(numbers []int, target int) []int {
+	l,r := 0, len(numbers)-1
+
+	for l < r {
+		sum := numbers[l] + numbers[r]
+
+		if target == sum {
+			return []int{l+1,r+1}
+		}
+
+		if target < sum {
+			r--
+		} else {
+			l++
+		}
+	}
+
+	return nil
+}
