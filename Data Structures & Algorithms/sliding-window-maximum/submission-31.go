188f556da64a4c4ade1a69f0d221a548099e860f
Rythmzz <101648696+Rythmzz@users.noreply.github.com>
2026-06-11 15:13:47 +0700

    Add: sliding-window-maximum - submission-31

---
+++ Data Structures & Algorithms/sliding-window-maximum/submission-31.go
@@ -0,0 +1,21 @@
+func maxSlidingWindow(nums []int, k int) []int {
+    deque := []int{}
+	result := []int{}
+
+	for i := 0; i < len(nums); i++ {
+		if len(deque) > 0 && i-k+1 > deque[0] {
+			deque = deque[1:]
+		}
+
+		for len(deque) > 0 && nums[i] > nums[deque[len(deque)-1]] {
+			deque = deque[:len(deque)-1]
+		}
+
+		deque = append(deque, i)
+		if i >= k-1 {
+			result = append(result, nums[deque[0]])
+		}
+	}
+
+	return result
+}
