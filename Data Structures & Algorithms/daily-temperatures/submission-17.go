8181e88b7722502fe87291174b5a4f094b05b072
Rythmzz <101648696+Rythmzz@users.noreply.github.com>
2026-06-17 11:23:06 +0700

    Add: daily-temperatures - submission-17

---
+++ Data Structures & Algorithms/daily-temperatures/submission-17.go
@@ -0,0 +1,17 @@
+func dailyTemperatures(temperatures []int) []int {
+	st := []int{}
+	result := make([]int, len(temperatures))
+	
+
+	for i:= 0; i< len(temperatures);i++ {
+		for len(st) > 0 && temperatures[i] > temperatures[st[len(st)-1]] {
+			idx := st[len(st)-1]
+			st = st[:len(st)-1]
+			result[idx] = i - idx
+		}
+
+		st = append(st,i)
+	}
+
+	return result
+}
